ec0879b4f06abb101db3615b8c6838485affdbb0
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-11 18:59:34 +0100

    Added more projects

--- personal-portfolio/src/components/Projects/projectData.tsx
+++ personal-portfolio/src/components/Projects/projectData.tsx
@@ -1,5 +1,8 @@
 import game from "../../assets/projects/2048.png";
 import punk from "../../assets/projects/punk.png";
+import calculator from "../../assets/projects/calculator.png";
+import yelpcamp from "../../assets/projects/yelpCamp.png";
+import sortingAlgo from "../../assets/projects/sortingAlgo.png";
 
 type Projects = {
   id: number;
@@ -12,38 +15,38 @@ type Projects = {
 const projectsData: Projects[] = [
   {
     id: 1,
+    name: "YelpCamp",
+    image: yelpcamp,
+    description: "This is a random project with a random image.",
+    url: "https://yelpcamp-obn9.onrender.com/",
+  },
+  {
+    id: 2,
     name: "2048 Clone",
     image: game,
     description: "A clone of the popular 2048 game.",
     url: "https://athishthayalan.github.io/2048/",
   },
   {
-    id: 2,
-    name: "PUNK API",
-    image: punk,
-    description: "An application utilizing the PUNK API.",
-    url: "https://athishthayalan.github.io/punkapi/",
-  },
-  {
     id: 3,
-    name: "Random Project 1",
-    image: "",
-    description: "This is a random project with a random image.",
-    url: "https://example.com/random-project-1",
+    name: "Sorting Algorithm Visualiser",
+    image: sortingAlgo,
+    description: "Yet another random project with a random image.",
+    url: "https://athishthayalan.github.io/sorting-algorithm-visualiser/",
   },
   {
     id: 4,
-    name: "Random Project 2",
-    image: "",
-    description: "This is another random project with a random image.",
+    name: "PUNK",
+    image: punk,
+    description: "An application utilizing the PUNK API.",
     url: "https://example.com/random-project-2",
   },
   {
     id: 5,
-    name: "Random Project 3",
-    image: "",
-    description: "Yet another random project with a random image.",
-    url: "https://example.com/random-project-3",
+    name: "Calculator",
+    image: calculator,
+    description: "Calculator",
+    url: "https://athishthayalan.github.io/calculator/",
   },
 ];
 
